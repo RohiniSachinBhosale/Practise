f347cb01ea73e9b71d93a93589a5aaedf5642fbe
RohiniSachinBhosale <101467864+RohiniSachinBhosale@users.noreply.github.com>
2022-10-14 12:48:38 +0530

    Add files via upload

---
+++ Datatype.cpp
@@ -0,0 +1,26 @@
+//coding challenge Day2
+//Data tyes in C++
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long l;
+    char ch;
+    double b;
+    float f;
+    
+    cin>>a;
+    cin>>l;
+    cin>>ch;
+    cin>>b;
+    cin>>f;
+    
+    
+    cout<<a<<" "<<l<<" "<<ch<<" "<<b<<" "<<f;
+    
+    return 0;
+}
